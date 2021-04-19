42da208e04c353c98249e2210378b8cbc66a7d9e
André <andrejmonteiro96@outlook.pt>
2021-04-19 02:35:57 +0100

    looking for players command WIP

--- bot/commands/users/lookingforplayers.js
+++ bot/commands/users/lookingforplayers.js
@@ -1,37 +1,31 @@
 const games = [
-	{ name: 'League of Legends', abbr: ['lol', 'league'] },
+	{ name: 'League of Legends', abbr: ['lol', 'league'], channel: 'league-of-legends' },
 	{ name: 'Sea of Thieves', abbr: ['sot'] },
 ];
 
-const aliases = ['lfp', 'nlfp'];
-
 module.exports = {
 	name: 'lookingforplayers',
-	aliases: aliases,
+	aliases: ['lfp'],
 	description: 'Permite procurar outros para jogar',
 	expectedArgs: '',
 	permissions: [],
 	permissionsErr: '',
 	requiredRoles: [],
-	execute: async (client, message, args, Discord) => {
+	execute: (client, message, args, Discord) => {
 		const role = message.guild.roles.cache.find(role => role.id === '803050640002908221');
-		if (aliases.includes(/^(lfp)/)) {
-			if (args[0]) {
-				message.member.roles.add(role.id);
-				const game = games.filter(games => games.abbr.includes(`${args[0]}`)).map(game => game.name);
-				message.reply(`@here, está  à procura de jogadores para **${game}**`);
-				return;
-			} else {
-				message.member.roles.add(role.id);
-				message.reply('@here, está  à procura de jogadores.');
-				return;
-			}
-		} else if (aliases.includes(/^(nlfp)/)) {
-			if (message.member.roles.cache.has(role.id)) {
-				message.member.roles.remove(role.id);
-				message.reply('já não está  à procura de jogadores.');
-				return;
-			}
-		}
+        if (args[0]) {
+            message.member.roles.add(role.id);
+            const game = games.filter(games => games.abbr.includes(`${args[0]}`)).map(game => game.channel);
+            const channel = message.channel.guild.channels.cache.find(ch => ch.name === `${game}`);
+            if (game && channel) {
+                channel.send(`@here, ${message.author} está  à procura de jogadores para **${game}**.`);
+                return;
+            }
+            message.channel.send(`@here, ${message.author} está  à procura de jogadores para **${game}**.`);
+            return;
+        } else {
+            message.member.roles.add(role.id);
+            message.reply('@here, está  à procura de jogadores.');
+        }
 	}
 }
