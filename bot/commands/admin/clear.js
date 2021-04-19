4857fdab5ddff5b778ee0d65b7daaa674410073b
André <andrejmonteiro96@outlook.pt>
2021-04-19 02:35:37 +0100

    removed async function

--- bot/commands/admin/clear.js
+++ bot/commands/admin/clear.js
@@ -5,27 +5,25 @@ module.exports = {
     expectedArgs: '!clear <numero:opcional>',
     permissions: ['ADMINISTRATOR', 'MANAGE_MESSAGES'],
     permissionsErr: '',
-    requiredRoles: [],
-    execute: async (client, message, args, Discord) => {
+    requiredRoles: ['796467134280237106'],
+    execute: (client, message, args, Discord) => {
         if (!args[0]) {
-            await message.channel.messages.fetch().then(messages => {
+            message.channel.messages.fetch().then(messages => {
                 message.channel.bulkDelete(messages);
                 if (process.env.MODE === 'Development') console.log(`Foram apagadas todas as mensagens do canal #${message.channel.name}!`);
             });
-            return;
         } else {
             if (isNaN(args[0])) {
-                await message.reply('Por favor utilize um número real.');
+                message.reply('Por favor utilize um número real.');
                 return;
             } else if (args[0] > 99) {
-                await message.reply('Não podem ser apagadas mais de 99 mensagens de cada vez.');
-                return; 
+                message.reply('Não podem ser apagadas mais de 99 mensagens de cada vez.');
+                return;
             } else {
-                await message.channel.messages.fetch({ limit: args[0] }).then(messages => {
+                message.channel.messages.fetch({ limit: args[0] }).then(messages => {
                     message.channel.bulkDelete(messages);
                     if (process.env.MODE === 'Development') console.log(`Foram apagadas ${args[0]} mensagens do canal #${message.channel.name}!`);
                 });
-                return;
             }
         }
     }
