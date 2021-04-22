3c2bb1b51e8dbf87bf09192e5db4b457c5e32479
André Monteiro <andrejmonteiro96@outlook.pt>
2021-04-22 19:05:47 +0100

    bot prints prefix on load

--- bot/events/client/ready.js
+++ bot/events/client/ready.js
@@ -1,6 +1,9 @@
-const { arrayRand } = require('../../config/functions');
+const { arrayRand } = require('../../config/functions.js');
+const { prefix } = require('../../config/config.json');
+
 
 module.exports = (Discord, client) => {
+    console.log(`${client.user.tag} está a usar o prefixo: ${prefix}`);
     console.log(`${client.user.tag} está agora online e a correr no modo de ${process.env.MODE}!`);
 
     const activities = [
