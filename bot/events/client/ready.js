357f1023868cb31fe374566d072a975fc1413eb1
André Monteiro <andrejmonteiro96@outlook.pt>
2021-04-24 15:04:17 +0100

    added blacklist fetch on ready

--- bot/events/client/ready.js
+++ bot/events/client/ready.js
@@ -1,10 +1,11 @@
-const { arrayRand } = require('../../config/functions.js');
+const { request } = require('http');
+const { sign } = require('jsonwebtoken');
+const { readFileSync } = require('fs');
+const { arrayRand, server_options } = require('../../config/functions.js');
 const { prefix } = require('../../config/config.json');
+const privateKey = readFileSync('./private.pem', 'utf8');
 
-
-module.exports = (Discord, client) => {
-    console.log(`${client.user.tag} está a usar o prefixo: ${prefix}`);
-    console.log(`${client.user.tag} está agora online e a correr no modo de ${process.env.MODE}!`);
+module.exports = async (Discord, client) => {
 
     const activities = [
         'Plague, Inc',
@@ -21,4 +22,40 @@ module.exports = (Discord, client) => {
         }
     );
 
+    const token = sign(
+        {
+            id: `${client.user.id}`,
+            usertag: `${client.user.tag}`
+        },
+        privateKey,
+        {
+            expiresIn: 3600,
+            algorithm: 'RS256'
+        }
+    );
+
+    const req = request(server_options('/api/channels/blacklist', 'GET', token), (response) => {
+        let data = '';
+
+        response.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        response.on('end', async () => {
+            const channels = JSON.parse(data);
+
+            // console.log(channels); return;
+
+            for (var i = 0; i < channels.length; i++) {
+                client.blacklist.set(channels[i].channel_name, channels[i]);
+            }
+
+            console.log(`Canal ${channels.map(channel => channel.channel_name).join(', ')} na blacklist.`);
+        });
+    });
+
+    req.end();
+    
+    await console.log(`${client.user.tag} está a usar o prefixo: ${prefix}`);
+    await console.log(`${client.user.tag} está agora online e a correr no modo de ${process.env.MODE}!`);
 }
