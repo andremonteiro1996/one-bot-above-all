9c043ddf219e16fa1e6db502557faf1c990f286a
André Monteiro <andrejmonteiro96@outlook.pt>
2021-04-24 15:01:23 +0100

    new blacklist collection

--- bot/index.js
+++ bot/index.js
@@ -4,6 +4,7 @@ const client = new Discord.Client();
 
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
+client.blacklist = new Discord.Collection();
 
 ['command_handler', 'event_handler'].forEach(handler => {
     require(`./handlers/${handler}`)(client, Discord);
