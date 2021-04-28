a17ce1d733ed2c0f306e8df4aacb8f49159ce728
André <andrejmonteiro96@outlook.pt>
2021-04-28 16:34:19 +0100

    added 2 new collections, for muted users and users on unmute queue

--- bot/index.js
+++ bot/index.js
@@ -5,6 +5,8 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
 client.blacklist = new Discord.Collection();
+client.mutedUsers = new Discord.Collection();
+client.usersOnUnmuteRoutine = new Discord.Collection();
 
 ['command_handler', 'event_handler'].forEach(handler => {
     require(`./handlers/${handler}`)(client, Discord);
