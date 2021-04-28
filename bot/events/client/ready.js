5d47d0de27655c44f1beba7ea62193ffc7963a75
André <andrejmonteiro96@outlook.pt>
2021-04-28 16:34:41 +0100

    new db requests for muted users - WIP

--- bot/events/client/ready.js
+++ bot/events/client/ready.js
@@ -34,24 +34,70 @@ module.exports = (Discord, client) => {
         }
     );
 
-    const req = request(server_options('/api/channels/blacklist', 'GET', token), (response) => {
+    const blacklistRequest = request(server_options('/api/channels/blacklist', 'GET', token), (response) => {
         let data = '';
 
         response.on('data', (chunk) => {
             data += chunk;
         });
 
-        response.on('end', async () => {
+        response.on('end', () => {
             const channels = JSON.parse(data);
-            for (var i = 0; i < channels.length; i++) {
-                client.blacklist.set(channels[i].channel_name, channels[i]);
+            if (channels.length !== 0) {
+                for (var i = 0; i < channels.length; i++) {
+                    client.blacklist.set(channels[i].channel_name, channels[i]);
+                }
+                console.log(`Canal ${channels.map(channel => channel.channel_name).join(', ')} na blacklist.`);
             }
-            console.log(`Canal ${channels.map(channel => channel.channel_name).join(', ')} na blacklist.`);
         });
     });
 
-    req.end();
-    
+    blacklistRequest.end();
+
+    const usersArr = [];
+
+    const guild = client.guilds.cache;
+    guild.each(guild => {
+        const users = guild.members.cache;
+        users.each(member => {
+            usersArr.push({
+                user_id: member.id,
+                user_tag: member.user.tag,
+                guild_id: guild.id,
+                guild_name: guild.name,
+                bot: member.user.bot
+            });
+        });
+    });    
+
+    const users = request(server_options('/api/users', 'POST', token), (response) => {
+        let data = '';
+
+        response.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        response.on('end', () => {});
+    });
+
+    users.write(JSON.stringify(usersArr));
+    users.end();
+
+    // const mutedUsers = request(server_options('/api/users/muted', 'GET', token), (response) => {
+    //     let data = '';
+
+    //     response.on('data', (chunk) => {
+    //         data += chunk;
+    //     });
+
+    //     response.on('end', () => {
+    //         const resp = JSON.parse(data);
+    //         console.log(resp.msg);
+    //     });
+    // });
+
+    // mutedUsers.end();
+
     console.log(`${client.user.tag} está a usar o prefixo: ${prefix}`);
     console.log(`${client.user.tag} está agora online e a correr no modo de ${process.env.MODE}!`);
 }
